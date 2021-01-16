eb9040a1ae3a319d4577de680a1328ad26e6ca6d
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-01-16 10:28:01 +0100

    Corrected spelling error in formula

--- calc.py
+++ calc.py
@@ -1,11 +1,30 @@
 def main():
-    forumla = input("What do you want to do? \n")
-    operator = forumla.index("+")
-    firstnum = int(forumla[operator:])
-    secondnum = int(forumla[:operator])
-    result = addition(firstnum, secondnum)
+    formula = input('What do you want to do? \n')
+    #op_index = get_operator(formula)
+    op_index = 1
+    operator = formula[op_index]
+    numbers = formula.split('-')
+    firstnum = float(numbers[0])
+    secondnum = float(numbers[1])
 
-    print(f"The answer is: {result}")
+    if operator == '+':
+        result = addition(firstnum, secondnum)
+    elif operator == '-':
+        result = subtraction(firstnum, secondnum)
+    elif operator == '*':
+        result = multiplication(firstnum, secondnum)
+    elif operator == '/':
+        result = division(firstnum, secondnum)
+    elif operator == '%':
+        result = modulo(firstnum, secondnum)
+    else:
+        print(f'No formula found: {formula}')
+
+    print(f'The answer is: {result}')
+
+
+def get_operator(user_formula):
+    return 1
 
 
 def addition(a, b):
@@ -14,6 +33,18 @@ def addition(a, b):
 
 def subtraction(a, b):
     return a - b
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
+
+
+def modulo(a, b):
+    return a % b
 
 
 if __name__ == '__main__':
