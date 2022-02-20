7abfb88cb0ea6125929e2fddcfdc6b18dde3334f
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-02-20 21:57:21 +0100

    Split formula by spaces.

--- calc.py
+++ calc.py
@@ -1,34 +1,27 @@
 def main():
-    """
     formula = input('What do you want to do? \n')
-    op_index = get_operator(formula)
-    operator = formula[op_index]
-    numbers = formula.split()
-    firstnum = convert_input(numbers[0])
-    secondnum = convert_input(numbers[1])
-    """
-    operator = input('What do you want to do? \n')
-    first_number = convert_input(input('Enter the first number: \n'))
-    second_number = convert_input(input('Enter the second number:  \n'))
+    components = formula.split()
+    if len(components) == 3:
+        operator = components[1]
+        first_number = convert_input(components[0])
+        second_number = convert_input(components[2]) 
 
-    if operator == '+':
-        result = addition(first_number, second_number)
-    elif operator == '-':
-        result = subtraction(first_number, second_number)
-    elif operator == '*':
-        result = multiplication(first_number, second_number)
-    elif operator == '/':
-        result = division(first_number, second_number)
-    elif operator == '%':
-        result = modulo(first_number, second_number)
+        if operator == '+':
+            result = addition(first_number, second_number)
+        elif operator == '-':
+            result = subtraction(first_number, second_number)
+        elif operator == '*':
+            result = multiplication(first_number, second_number)
+        elif operator == '/':
+            result = division(first_number, second_number)
+        elif operator == '%':
+            result = modulo(first_number, second_number)
+        else:
+            print(f'No formula found: {formula}')
+
+        print(f'The answer is: {result}')
     else:
         print(f'No formula found: {formula}')
-
-    print(f'The answer is: {result}')
-
-
-def get_operator(user_formula):
-    return 1
 
 
 def convert_input(user_num):
